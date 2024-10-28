c8bc89b038f283acf36e2542fd75c47a17cbc6aa
Zecyel <zecyel@163.com>
2024-10-28 18:45:16 +0800

    fix: check user permission in middleware

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,6 +1,7 @@
 import type { IUser } from '@/server/models/User'
 import type { IncomingMessage, ServerResponse } from 'node:http'
 import { initTRPC } from '@trpc/server'
+import * as cookie from 'cookie'
 
 // const t = initTRPC.create()
 const t = initTRPC.context<{
@@ -14,28 +15,49 @@ export const router = t.router
 export const middleware = t.middleware
 export const mergeRouters = t.mergeRouters
 
-export const isAuthenticated = t.middleware(({ ctx, next }) => {
-  if (!ctx.user) {
+export const isAuthenticated = t.middleware(async ({ ctx, next }) => {
+  const parsedCookie = cookie.parse(ctx.req.headers.cookie || '')
+  const token = parsedCookie['auth-token']
+
+  if (!token) {
     throw new Error('未认证，请登录')
   }
+
+  const user = await getUserFromToken(token)
+
+  if (!user) {
+    throw new Error('用户未找到，特殊情况已截流上传')
+  }
+
   return next({
     ctx: {
-      user: ctx.user,
+      user: await getUserFromToken(token),
     },
   })
 })
 
 export const protectedProcedure = publicProcedure.use(isAuthenticated)
 
-export const isAdmin = t.middleware(({ ctx, next }) => {
-  if (ctx.user?.role !== 'admin') {
+export const isAdmin = t.middleware(async ({ ctx, next }) => {
+  const parsedCookie = cookie.parse(ctx.req.headers.cookie || '')
+  const token = parsedCookie['auth-token']
+
+  if (!token) {
+    throw new Error('未认证，请登录')
+  }
+
+  const user = await getUserFromToken(token)
+
+  if (!user) {
+    throw new Error('用户未找到，特殊情况已截流上传')
+  }
+
+  ctx.user = user
+
+  if (ctx.user.role !== 'admin') {
     throw new Error('无权限，请使用管理员账号登录')
   }
-  return next({
-    ctx: {
-      user: ctx.user,
-    },
-  })
+  return next({ ctx })
 })
 
 export const adminProcedure = publicProcedure.use(isAdmin)
